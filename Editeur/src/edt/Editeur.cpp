efe0371df3b09f7ea092f2c5f8fdf548ff05caa0
Matthieu Zimmer <zimme0511@etumail.uhp-nancy.fr>
2012-05-01 21:36:43 +0200

    moving maps is now possible through SFML

--- Editeur/src/edt/Editeur.cpp
+++ Editeur/src/edt/Editeur.cpp
@@ -14,7 +14,7 @@ namespace edt {
 
 Editeur::Editeur()
 {
-    
+
 }
 
 Editeur::~Editeur()
@@ -26,7 +26,7 @@ void Editeur::init(const string& path_carte) {
 
     fenetre = new sf::RenderWindow(sf::VideoMode( 1600, 900 ), "Hunters' World", sf::Style::Default );
     fenetre->setMouseCursorVisible(false);
-    
+
     fenetre->setFramerateLimit(60);
 
     GUI::getInstance()->init();
@@ -44,41 +44,70 @@ void Editeur::run() {
     {
         GUI::getInstance()->tocHorloge();
 
-        sf::Event Event;
-        while (fenetre->pollEvent(Event))
+        sf::Event event;
+        while (fenetre->pollEvent(event))
         {
-
-            if (GUI::getInstance()->captureEvent(Event)) {
+            bool pressed;
+            int x, y;
+            if (GUI::getInstance()->captureEvent(event)) {
                 // Décommenter pour ignorer les events utilisés par CEGUI dans SFML
                 //continue;
             }
 
+            if(event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Button::Right) {
+                pressed = true;
+                x = event.mouseButton.x;
+                y = event.mouseButton.y;
+            }
 
-            if (Event.type == sf::Event::Closed){
+            if(pressed && event.type == sf::Event::MouseMoved) {
+                int dx = (x - event.mouseMove.x);
+                int dy = (y - event.mouseMove.y);
+                if(vue->getCenter().x + dx < vue->getSize().x/2)
+                    dx = 0;
+                else if(vue->getCenter().x + dx > carte->getCoucheTileEditable()->getLargeur()*158 - vue->getSize().x/2 + 88)
+                    dx=0;
+
+                if(vue->getCenter().y + dy < vue->getSize().y/2)
+                    dy = 0;
+                else if(vue->getCenter().y + dy > carte->getCoucheTileEditable()->getHauteur()*44 - vue->getSize().y/2 + 44)
+                    dy=0;
+
+                vue->setCenter(vue->getCenter().x + dx, vue->getCenter().y + dy);
+
+                x = event.mouseMove.x;
+                y = event.mouseMove.y;
+            }
+
+            if(pressed && event.type == sf::Event::MouseButtonReleased && event.mouseButton.button == sf::Mouse::Button::Right) {
+                pressed = false;
+            }
+
+            if (event.type == sf::Event::Closed) {
                 fenetre->close();
             }
         }
 
         fenetre->clear();
         fenetre->setView(*vue);
-	
-	fenetre->pushGLStates();
+
+        fenetre->pushGLStates();
         carte->dessinerSur(*fenetre);
-	fenetre->popGLStates();
-	
-	
+        fenetre->popGLStates();
+
+
         GUI::getInstance()->dessiner();
         fenetre->display();
     }
 }
 
-sf::View* Editeur::getVueCourante(){
-  return vue;
+sf::View* Editeur::getVueCourante() {
+    return vue;
 }
 
 edt::CarteEditable* Editeur::getCarteCourante()
 {
- return carte; 
+    return carte;
 }
 
 } /* End of namespace edt */
